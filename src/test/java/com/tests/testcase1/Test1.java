146f3a61b83e85f14e353e993895801ade71d06a
Eugene777zp <Imarkadanov@gmail.com>
2024-07-19 15:05:20 -0400

    third commit

--- src/test/java/com/tests/testcase1/Test1.java
+++ src/test/java/com/tests/testcase1/Test1.java
@@ -1,11 +1,11 @@
-package data;
+package com.tests.testcase1;
 
 import org.testng.annotations.Test;
 
 public class Test1 {
 
     @Test
-    public void test1(){
+    public void testingPrint(){
         System.out.println("Running Test Case 1");
     }
 }
